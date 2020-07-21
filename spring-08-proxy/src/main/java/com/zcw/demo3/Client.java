0c8d77e737839425888d1a164c3949e2fcb40cfe
zcwshu <1107210105@qq.com>
2020-07-21 20:04:07 +0800

    静态代理和动态代理

---
+++ spring-08-proxy/src/main/java/com/zcw/demo3/Client.java
@@ -0,0 +1,20 @@
+package com.zcw.demo3;
+
+/**
+ * @Author: AndrewBar
+ * @Date: Created in 13:05 2020/7/21
+ */
+public class Client {
+    public static void main(String[] args) {
+        //真实角色
+        Host host = new Host();
+
+        //代理角色 : 现在没有
+        ProxyInvocationHandler pih = new ProxyInvocationHandler();
+
+        //通过调用程序处理角色,来处理我们要调用的接口对象
+        pih.setRent(host);
+        Rent proxy = (Rent) pih.getProxy();//调用getProxy自动生成
+        proxy.rent();
+    }
+}
